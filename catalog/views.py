a9073076c062d1a318fe21a5b55047d5cea09a6f
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-05-17 14:12:03 +0300

    complete 3 to 5 parts of the guide

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+
+def index(request):
+    """
+    Function to display the home page of the website.
+    """
+    # Generating "counts" of some main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # 'all()' method is applied by default.
+    # Rendering the HTML template index.html with data inside
+    # the context variable context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+    )
