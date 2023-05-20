e170c9e2b8450f8634add9bd44df5c584f8b5345
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-05-20 22:22:33 +0300

    clean up code

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
 from django.urls import re_path
+
+from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),  # mb I should use path, not re_path
