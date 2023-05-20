50a940aeedda0f7cb501add10efbc4c881771918
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-05-20 22:21:38 +0300

    complete part 6 of guide, add views of books, authors and their detailed info

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.urls import re_path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    re_path(r'^$', views.index, name='index'),  # mb I should use path, not re_path
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
